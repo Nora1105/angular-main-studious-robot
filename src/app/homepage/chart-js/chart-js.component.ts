ef8c7ec6e05d1f6e1bb2d7e609e273e663e8ace4
aaron <vargheeseaaronv@gmail.com>
2024-04-01 10:32:13 +0530

    chart added

--- src/app/homepage/chart-js/chart-js.component.ts
+++ src/app/homepage/chart-js/chart-js.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-chart-js',
@@ -6,5 +7,68 @@ import { Component } from '@angular/core';
   styleUrl: './chart-js.component.css'
 })
 export class ChartJsComponent {
+  constructor() { }
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  public chart: any;
+
+  createChart() {
+    this.chart = new Chart("MyChart", {
+      // type: 'pie',
+      type: 'doughnut',
+      // type: 'radar',
+
+      data: {
+        labels: [
+          'Wheat',
+          'Maize',
+          'Rice',
+          'Sugarcane',
+          'Cotton'
+        ],
+        datasets: [{
+          label: 'Area and Production of Important Crops (2020-21)',
+          data: [9168.2, 1417.8, 3335.1, 1165.0, 2078.9],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(153, 102, 255)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        aspectRatio: 2.5,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Area and Production of Important Crops (2020-21)',
+            font: {
+              size: 24,
+              weight: 'bold',
+              family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+            },
+            padding: {
+              top: 10,
+              bottom: 30
+            }
+          },
+          legend: {  
+            display: true,
+            labels: {
+              font: {
+                size: 14,
+                family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 }
