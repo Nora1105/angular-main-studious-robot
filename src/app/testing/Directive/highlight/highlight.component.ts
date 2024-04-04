8a6cfd21ba856d984fff5ceffe6359a7e072b23d
aaron <vargheeseaaronv@gmail.com>
2024-04-04 11:18:12 +0530

    added nonBindable to directive component

--- src/app/testing/Directive/highlight/highlight.component.ts
+++ src/app/testing/Directive/highlight/highlight.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './highlight.component.css'
 })
 export class HighlightComponent {
-
+  color='';
 }
--- src/app/testing/Directive/lighter.directive.ts
+++ src/app/testing/Directive/lighter.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef,  HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appLighter]'
@@ -6,7 +6,30 @@ import { Directive, ElementRef } from '@angular/core';
 export class LighterDirective {
 
   constructor(private el:ElementRef) {
-    this.el.nativeElement.style.backgroundColor='yellow';
+    //  //sets background-color to yellow on loading
+    // this.el.nativeElement.style.backgroundColor='yellow';
    }
 
+  //  //sets background color to yellow on mouseover
+  //  @HostListener('mouseenter') onMouseEnter() {
+  //   this.highlight('yellow');
+  // }
+  
+  // @HostListener('mouseleave') onMouseLeave() {
+  //   this.highlight('');
+  // }
+  
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  @Input() appLighter='';
+  @Input() defaultColor='';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appLighter || this.defaultColor || 'red');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+      this.highlight('');
+    }
 }
