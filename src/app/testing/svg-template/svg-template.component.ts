5289ae6b33e3d82ed709f9ae588d3da2aa999e1b
aaron <vargheeseaaronv@gmail.com>
2024-04-08 18:05:05 +0530

    minor changes

--- src/app/testing/svg-template/svg-template.component.ts
+++ src/app/testing/svg-template/svg-template.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-svg-template',
   templateUrl: './svg-template.component.html',
   styleUrl: './svg-template.component.css'
 })
 export class SvgTemplateComponent {
-  fillColor = 'rgb(255,0,0)';
+  fillColor = 'rgb(125,125,125)';
   changeColor(){
     const r = Math.floor(Math.random()*256);
     const g = Math.floor(Math.random()*256);
     const b = Math.floor(Math.random()*256);
     this.fillColor=`rgb(${r},${g},${b})`;
   }
-
   text='';
   onKeydown(){
     const te = "Key Pressed";
